84ad19c2679c71ef7d8b2212df77e504e1960388
meganii <meganii@outlook.jp>
2025-05-02 19:52:41 +0900

    Update

--- content/direct-access/direct-access.ts
+++ content/direct-access/direct-access.ts
@@ -1,6 +1,10 @@
+console.log("direct-access");
 
-console.log('direct-access');
-
-setTimeout(function() {
-    document.getElementById("direct-link")?.click();
-}, 100);
+if (location.search === "?edit") {
+    console.log('edit mode');
+    history.pushState({}, "", location.pathname);
+} else {
+    setTimeout(function () {
+        document.getElementById("direct-link")?.click();
+    }, 100);
+}
--- content/direct-access/index.md
+++ content/direct-access/index.md
@@ -2,7 +2,7 @@
 title: "Direct access to cosense"
 ---
 
-<a id="direct-link" href="https://scrapbox.io/meganii-private">リンク</a>
+<a id="direct-link" href="https://scrapbox.io/meganii-private/">リンク</a>
 
 {{< script src="direct-access.ts" >}}
 
