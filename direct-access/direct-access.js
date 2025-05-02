e24761fc3e1413f726fe3bfae13da22c76bfee6d
meganii <meganii@users.noreply.github.com>
2025-05-02 10:53:06 +0000

    deploy: 84ad19c2679c71ef7d8b2212df77e504e1960388

--- direct-access/direct-access.js
+++ direct-access/direct-access.js
@@ -1,7 +1,12 @@
 (() => {
   // <stdin>
   console.log("direct-access");
-  setTimeout(function() {
-    document.getElementById("direct-link")?.click();
-  }, 100);
+  if (location.search === "?edit") {
+    console.log("edit mode");
+    history.pushState({}, "", location.pathname);
+  } else {
+    setTimeout(function() {
+      document.getElementById("direct-link")?.click();
+    }, 100);
+  }
 })();
